b5197e614b927719a95b8688f457a0eb1903d2c6
Emily Asch <e.asch@me.com>
2018-10-10 19:09:33 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -15,15 +15,15 @@ def array_with_two_elements
 end
 
 def first_element
-  arr.at(0)
+  arr[1]
 end
 
 def third_element
-  arr.at(2)
+  arr[2]
 end
 
 def last_element
-  arr.at(-1)
+  arr[-1]
 end
 
 def first_element_with_array_methods
